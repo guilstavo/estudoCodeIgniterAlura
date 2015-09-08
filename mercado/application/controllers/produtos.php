e0bcc7bbd8b4c90610af261097b8a63ac5335194
guilstavo <gustavomartinis@gmail.com>
2015-09-07 22:44:32 -0300

    Profiling

--- mercado/application/controllers/produtos.php
+++ mercado/application/controllers/produtos.php
@@ -3,14 +3,13 @@
 
 		public function index(){
 			
-			$this->load->database();
 			$this->load->model('produtos_model');
 			$produtos = $this->produtos_model->buscaTodos();
 
 			$dados = array('produtos' => $produtos);
-			$this->load->helper('url');
-			$this->load->helper('currency');
-			$this->load->helper('form');
+			$this->load->helper(array('url'));
+			$this->load->helper(array('currency'));
+			$this->load->helper(array('form'));
 			$this->load->view('produtos/index.php', $dados);
 		}
 	}
