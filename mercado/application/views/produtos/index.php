48456b4cf6463ff761b483add8f82812b3603ab6
guilstavo <gustavomartinis@gmail.com>
2015-09-07 23:25:50 -0300

    Implementando o Login

--- mercado/application/views/produtos/index.php
+++ mercado/application/views/produtos/index.php
@@ -14,9 +14,35 @@
 			</tr>
 			<?php endforeach ?>
 		</table>
+		<h1>Login</h1>
+		<?php
+			echo form_open('login/autenticar');
+			echo form_label("Email", "email");
+			echo form_input(array(
+				'name' => 'email',
+				'id' => 'email',
+				'class' => 'form-control',
+				'maxlenght' => '255'
+				
+			));
+			echo form_label("Senha", "senha");
+			echo form_password(array(
+				'name' => 'senha',
+				'id' => 'senha',
+				'class' => 'form-control',
+				'maxlenght' => '255'
+				
+			));
+			echo form_button(array(
+				'class' => 'btn btn-primary',
+				'content' => 'Login',
+				'type' => 'submit'
+			));
+			echo form_close();
+		?>
 		<h1>Cadastro</h1>
 		<?php
-			echo form_open('usuarios/open');
+			echo form_open('usuarios/novo');
 			echo form_label("Nome", "nome");
 			echo form_input(array(
 				'name' => 'nome',
